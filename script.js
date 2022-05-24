282d11777f533d0795f22d14d32f280ffff04dcd
Dean Thompson <deanthompson.music@gmail.com>
2022-05-24 13:50:55 +0900

    Added music to site

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const mySong = document.getElementById("mySong");
+var icon = document.getElementById("icon");
+
+icon.onclick = function() {
+    if(mySong.paused) {
+        mySong.play();
+        icon.src = "media/White Pause Button Symbol Png.png";
+    } else {
+        mySong.pause();
+        icon.src = "media/Play Button Png.png";
+    }
+    m
+}
+
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener("click", function(e) {
         e.preventDefault();
@@ -6,3 +21,4 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
