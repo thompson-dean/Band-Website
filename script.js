a511861725aa65217835dfe0196a299e0be9e262
Dean Thompson <deanthompson.music@gmail.com>
2022-12-04 13:28:21 +0900

    add apple music spotify and amazon

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 window.onload = function () {
     const menu_btn = document.querySelector('.hamburger')
     const mobile_menu = document.querySelector('.mobile-nav')
+    const promotion_button = document.querySelector('.buy-album')
 
     menu_btn.addEventListener('click', function () {
         menu_btn.classList.toggle('is-active')
         mobile_menu.classList.toggle('is-active')
+        promotion_button.classList.toggle('is-active')
     });
 
     //Handle Animation
