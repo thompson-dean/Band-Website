7daff86daac99591ded64bca16826db7e3a66f7f
Dean Thompson <deanthompson.music@gmail.com>
2024-07-27 15:07:30 +0900

    add translation, fix burger bug

--- script.js
+++ script.js
@@ -1,30 +1,92 @@
+// Constants
+const japaneseBlurb = "多国籍トリオによる国とジャンルを超えた音楽の融合。"
+const englishBlurb = "A meeting of 3 minds - a blend of classical, jazz, funk, RnB, fusion, and world beats."
+
+const englishProfile = "Trivergents are a band comprising international musicians, each bringing their unique cultural rhythms and melodies to create a transcendent musical experience. Blending classical, jazz, funk, RnB, fusion, and world beats, Trivergents break boundaries and defy genres, crafting a sound that is rich, engaging, and appealing to listeners of all backgrounds. <br><br> While their technical musicianship and depth of knowledge from both the classical and jazz disciplines are clearly present, these aspects are not the overt focus of the band. Rather, Trivergents aim to evoke powerful emotions and experiences in their listeners—be it nostalgia, passion, or an insatiable urge to dance. Their music, characterized by clever yet tasteful use of time and harmony with precise execution, is crafted to be accessible and enjoyable to all, whether they are musicians or not."
+
+const japaneseProfile = "Trivergentsは国際的に活躍する多国籍ミュージシャンで構成されるバンドである。それぞれが独自に持つ文化に立脚したリズム、ハーモニー、メロディーを掛け合わせ、今までに類を見ない音楽体験を創り出す。クラシック、ジャズ、ファンク、RnB、フュージョン、ワールド・ビートを融合させ、ジャンルの枠を華麗に飛び越え、多様な音楽的バックグラウンドを持つリスナーを唸らせる色鮮やかなサウンドを作り上げている。2022年初頭にリリースされたデビュー・アルバム「Transfer」に収録されているクラシック曲のアレンジやオリジナル曲は、彼らの創造性と職人技とも言える演奏技術を惜しみなく発揮している。"
+
+const englishMichal = "Born in Poland, graduated from Frederyk Chopin University of Music. He is jury member at PTNA, the International Chopin Piano Competition in ASIA and the jury chairman of European International Piano Concours in Japan. In 2021, he founded the Japan Jazz Pop Piano Competition. In May 2022, he performed with Kanagawa Philharmonic Orchestra for a charity concert for Ukraine."
+
+const japaneseMichal = "ポーランドの音楽家の家庭に生まれる。10歳でテレビ番組「Akademia muzyczna （アカデミア・ムジチナ）」に出演しピアニストとしてデビュー。その後、ポーランド国立フィルハーモニー・ホー ル等多くのコンサート・ホールで演奏し、海外の国際音楽祭にも多数参加する。作曲家、ジャズ・ピアニストと しても活躍。ワルシャワ・ショパン音楽院 (現・ ショパン音楽大学)ピアノ科卒業後、チューリッヒ音楽院研究科留学。アンジェイ・ヤシンスキ、イェジー・マルクウィンスキ、テレサ・マナステルスカ、ガブリエラ・ワイスの各氏に師事。1995年フランツ・リスト国際ピアノコンクール（ポーランド）入賞。1996年フレデリック・ショパン協会（ワルシャワ）より奨学金を得る。1997年ヨーロッパ・ピアノフォーラム(ベルリン)に出演。2002年第36回モントルー・ジャズ・フェスティバル(スイス)のプレスルームで演奏。2017年にリリースしたCD「Jazz Loves Chopin」は e-onkyo music の Top 100 Album ランキングで1位を獲得。2018年公開の映画「羊と鋼の森」にピアニスト役として出演。2022年5月に開催された「ウクライナ人道支援チャリティー・コンサート」にて神奈川フィルハーモニー管弦楽団と演奏。現在、福島学院大学教授、昭和音楽大学講師。ピティナ、ショパン国際ピアノコンクール in ASIAの審査員、ヨーロッパ国際ピアノコンクール in Japanの審査員長。Japan Jazz Pop Piano Competitionの創立者、及び審査員長。"
+
+const englishDanny = "Scottish born bassist/composer. Toured/recorded with Ayub Ogada (Real World Records) jazz trumpeter Kevin Davy (Lamb/D-semble), and Grammy nominated guitarist David Becker. Co-wrote with Soul artist Jehlisa Anderson (Warner Bros), composed for television, multimedia and game titles  (Konami and Sega). Currently runs Bass Japan Direct in addition to doing sessions / composing."
+
+const japaneseDanny = "1974年スコットランドのエディンバラで生まれる。20歳の時にベースギターを弾き始め、1997年ロンドンにてプロとして活動を開始する。ケニアの偉大なアーティストである故Ayub OgadaやWarner BrosのRnBアーティストJehlisa、著名なプロデューサーであるJamie Katoなど、幅広いジャンルで活躍しているアーティストとともにツアーやレコーディングを行う。RealworldやAbbey Roadなど有名スタジオでのレコーディングも数多く経験している。結婚を機に、2003年に東京に移住。CM、ゲームなどの作曲、サウンドデザインを中心に活動。2013年にSTRやバッカス他国内ギターメーカーのデモンストレーションを行うなど、活動は多岐に渡る。2015年に日本製のベースギター輸出会社Bass Japan Directを設立し、そのYoutubeチャンネルの閲覧数はミリオンビューをはるかに超え、今では千本以上の動画がアップロードされている。2019年にはグラミー賞にノミネートされたアメリカのギタリストDavid Beckerと日本ツアーを行う。現在はヨーロッパメーカーを始めTRIBEギターとNUX FXエフェクトペダルをサポートし、定期的に作曲、アレンジ、演奏を続けている。"
+
+const englishDean = "Dean is first and foremost a percussionist, who spent many years teaching drums and touring Australia as a professional musician playing Jazz and Funk primarily, but also other amalgams of contemporary music, such as, Latin, Electronica and World Music. During his time in Australia he played, cowrote and recorded music with Josue Vilches, and played with the video game jazz cover band The Consouls. Now Dean lives in Tokyo and is gigging with world renowned musicians such as Michal Sobkowiak, Harvey Thompson and David Becker."
+
+const japaneseDean = "オーストラリア出身、東京在住。リズムの多様性と創造性に強く焦点を当てた多才なドラマー。今まで多くの学生にドラムを指導し、オーストラリアで様々なアーティストと共にツアーを行う。ジャズ、ファンク、ラテン、エレクトロニカ、ワールド・ミュージックなどを主に演奏する。Josue Vilchesと共同で音楽を制作し、ビデオゲーム・ジャズカバーバンド「The Consouls」のドラマーとして活動する。現在はHarvey ThompsonやDavid Beckerなどの世界トップクラスのミュージシャンと共演し、常に新しい音楽の可能性を追求している。"
+
+const translations = {
+    "title": { "en": "Trivergents", "ja": "トライヴァージェンツ" },
+    "description": { "en": englishBlurb, "ja": japaneseBlurb },
+    "profile": { "en": englishProfile, "ja": japaneseProfile },
+    "michal": { "en": englishMichal, "ja": japaneseMichal },
+    "danny": { "en": englishDanny, "ja": japaneseDanny },
+    "dean": { "en": englishDean, "ja": japaneseDean },
+};
+
 window.onload = function () {
-    const menu_btn = document.querySelector('.hamburger')
-    const mobile_menu = document.querySelector('.mobile-nav')
-    const promotion_button = document.querySelector('.buy-album')
+    const menu_btn = document.querySelector('.hamburger');
+    const mobile_menu = document.querySelector('.mobile-nav');
+    const promotion_button = document.querySelector('.buy-album');
+    const menu_items = document.querySelectorAll('.mobile-nav a');
+    const langSwitch = document.getElementById('langSwitch');
+    let currentLang = 'en';
 
-    menu_btn.addEventListener('click', function () {
-        menu_btn.classList.toggle('is-active')
-        mobile_menu.classList.toggle('is-active')
-        promotion_button.classList.toggle('is-active')
-    });
+    function updateFlags() {
+        const enFlags = document.querySelectorAll('#enFlagMain, #enFlagMobile');
+        const jaFlags = document.querySelectorAll('#jaFlagMain, #jaFlagMobile');
+        
+        enFlags.forEach(flag => flag.style.display = currentLang === 'ja' ? 'inline' : 'none');
+        jaFlags.forEach(flag => flag.style.display = currentLang === 'en' ? 'inline' : 'none');
+    }
 
-    //Handle Animation
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener("click", function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute("href")).scrollIntoView({
-            behavior: "smooth"
+    function setInitialContent() {
+        document.querySelector('.hero .content p').textContent = translations.description[currentLang];
+        document.querySelector('.profile').innerHTML = translations.profile[currentLang];
+        document.querySelector('.michal').textContent = translations.michal[currentLang];
+        document.querySelector('.danny').textContent = translations.danny[currentLang];
+        document.querySelector('.dean').textContent = translations.dean[currentLang];
+    }
+
+    updateFlags();
+    setInitialContent();
+
+    function toggleMenu() {
+        menu_btn.classList.toggle('is-active');
+        mobile_menu.classList.toggle('is-active');
+        promotion_button.classList.toggle('is-active');
+    }
+
+    menu_btn.addEventListener('click', toggleMenu);
+
+    menu_items.forEach(item => {
+        item.addEventListener('click', function() {
+            if (mobile_menu.classList.contains('is-active')) {
+                toggleMenu();
+            }
         });
     });
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener("click", function(e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute("href")).scrollIntoView({
+                behavior: "smooth"
+            });
+        });
+    });
+
+    function switchLanguage() {
+        currentLang = currentLang === 'en' ? 'ja' : 'en';
+        updateFlags();
+        setInitialContent();
+    }
+
+    const langSwitchButtons = document.querySelectorAll('#langSwitchMain, #langSwitchMobile');
+    langSwitchButtons.forEach(button => {
+    button.addEventListener('click', switchLanguage);
 });
-}
-
-
-
-
-
-
-
-
-
+};
